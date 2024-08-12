341527cb27f1bf0e7880f944c431ad7ddaff6c80
Kaarti22 <kaartimondi@gmail.com>
2024-08-12 16:17:18 +0530

    created posts app and layout for templates

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin # type: ignore
+from django.urls import path, include # type: ignore
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.homePage),
     path("about/", views.about),
+    path('posts/', include('posts.urls')),
 ]
