a1fe1c8e088296bd8016e0ccdb82c3ff4ac50fef
Kaarti22 <kaartimondi@gmail.com>
2024-08-12 16:31:30 +0530

    created post model and applied all migrations

--- myproject/posts/models.py
+++ myproject/posts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add = True)
